30fa7299e7a29efe3d7001ae7eea6daa2834b065
Akshay Deshmukh <akshaydeshmukh@onetab.ai>
2026-09-15 18:20:25 +0530

    Delete jest.config.ts

--- jest.config.ts
+++
@@ -1,9 +0,0 @@
-import type { Config } from 'jest';
-const config: Config = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  roots: ['<rootDir>/src'],
-  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node']
-};
-export default config;
